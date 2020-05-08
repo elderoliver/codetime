ea1b158f3601c7d7cfdb84348e27c30c5769fffd
Elder Oliveira Castro <elder.oliveira@epoca.dc>
2020-05-08 18:04:50 -0300

    Best index.js

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,6 +5,7 @@ export default function Main(){
 
     const [time, setTime] = useState('00:00:10');  
 
+    let saveStartTime = ''; 
 
     const [dataTable,setDataTable] = useState([{
         description: 'Start develop the application',
@@ -21,8 +22,12 @@ export default function Main(){
     }]);
 
     function play(){
+
         var startDate = new Date("January 31 1980 "+time);
+        saveStartTime = time; 
+
         var finalDate = new Date("January 31 1980 00:00:00");
+
         var i = setInterval(function(){
             startDate.setSeconds(startDate.getSeconds() - 1);             
             setTime(startDate.getHours()+":"+startDate.getMinutes()+":"+startDate.getSeconds());
@@ -34,6 +39,7 @@ export default function Main(){
                     time: '20 min'
                 }; 
                 setDataTable([...dataTable,newRow]); 
+                setTime(saveStartTime); 
                 clearInterval(i);
 
             }
@@ -66,18 +72,9 @@ export default function Main(){
                         <th>Time</th>
                     </tr>
 
-                    {/* <tr>
-                        <td>Start develop the application</td>
-                        <td>06/05/2020 15:44</td>
-                        <td>20 min</td>
-                    </tr>
-                    <tr>
-                        <td>Debug application</td>
-                        <td>06/05/2020 17:00</td>
-                        <td>20 min</td>
-                    </tr> */}
-
-                    { dataTable.map( (dt) => (
+                    { 
+                        
+                        dataTable.map( (dt) => (
                         
                         <tr>
                             <td>{ dt.description }</td>
